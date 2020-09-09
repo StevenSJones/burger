5318145ce43b5b5aeb55fdda19aacbc6ed0d6a78
SS Jones <SSJones.code@gmail.com>
2020-09-09 16:32:50 -0700

    set the port and allow the port to be set by heroku.

--- server.js
+++ server.js
@@ -2,4 +2,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
-
+//creating and calling the express server instance
+const app = express();
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
