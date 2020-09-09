e80f281def620c2c2d0fa1259be7c615a0094a01
SS Jones <SSJones.code@gmail.com>
2020-09-09 16:44:44 -0700

    created the burgers table in the schema.db.

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,11 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 -- Uses the database
 USE burgers_db;
+--  2. Inside of that database, make a table called "wishes" which will have a wish column and an id column. The id will be automatically incremented while also being the primary key.
+-- Create the table tasks.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger varchar(255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
