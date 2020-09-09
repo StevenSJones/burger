6983c2910b063f9cb2cd9540ae7aa167f50af930
SS Jones <SSJones.code@gmail.com>
2020-09-09 16:42:05 -0700

    I added the drop db, create db and use db with the burgers_db being the name of the db.

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+-- Drops the task_saver_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+-- Create the database task_saver_db and specified it for use.
+CREATE DATABASE burgers_db;
+-- Uses the database
+USE burgers_db;
