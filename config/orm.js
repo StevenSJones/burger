4983466a726d4aa1f76fc8746880427926709a82
SS Jones <SSJones.code@gmail.com>
2020-09-10 15:31:03 -0700

    I called the orm funcitons in the burger.js.

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,5 @@
 //    * Import (require) `connection.js` (the sql connection) into `orm.js`
+const connection = require("connection");
 
 //    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
@@ -9,9 +10,18 @@
 //    * Export the ORM object in `module.exports`.
 
 // Import MySQL connection. In this case iporting from the connection.js file
-var connection = require("../config/connection.js");
+//?? indicates a table OR columns and ? indicates a single point of data
+const orm = {
+    selectAll: function (tableInput, cb) {
+      const queryString = "SELECT * FROM ?? " + tableInput + ";";
+      connection.query(queryString, function (err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    }
+}
 
-
-// function selectAll(){
-
-// }
+//    * Export the ORM object in `module.exports`.
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,34 @@
+// Inside your `burger` directory, create a folder named `models`.
+
+//   * In `models`, make a `burger.js` file.
+
+//     * Inside `burger.js`, import `orm.js` into `burger.js`
+
+
+
+// * Inside `burger.js`, import `orm.js` into `burger.js`
+const orm = require("../config/orm.js");
+
+// * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    //calling the update function from the orm
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+//     * Export at the end of the `burger.js` file.
+module.exports = burger;
