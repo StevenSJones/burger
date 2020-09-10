cfc8162359bed42c43d2e5b562086dadb65092f7
SS Jones <SSJones.code@gmail.com>
2020-09-09 19:36:11 -0700

    I created the orm.js file inside of the config directory and inserted the instructions in the form of comments.

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@
 //* Inside the `connection.js` file, setup the code to connect Node to MySQL.
 
 //* Export the connection.
+
 //import our npm packages
 const mysql = require("mysql");
 //this describes the connection to-be of the sql server(dialing the number to make the connection)
