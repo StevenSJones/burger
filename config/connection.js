1e80823ba3157bdc69150c3860b06b23c4de5818
SS Jones <SSJones.code@gmail.com>
2020-09-09 19:32:24 -0700

    I ammended the server.js and connection.js files to match the instructions as I got ahead of myself. Next step is setting up the orm file.

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// #### Config Setup
+// 2. Create a `connection.js` file inside `config` directory.
+
+//* Inside the `connection.js` file, setup the code to connect Node to MySQL.
+
+//* Export the connection.
+//import our npm packages
+const mysql = require("mysql");
+//this describes the connection to-be of the sql server(dialing the number to make the connection)
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "pets_db"
+});
+//Using the sql server (pressing the green call button)
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+//exports the mysql server connection 
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-//import our npm modules
+// #### App Setup
+// 7.Require the following npm packages inside of the server.js file: * express
+//import our npm packages
 const express = require("express");
-const exphbs = require("express-handlebars");
-const mysql = require("mysql");
-//creating and calling the express server instance
-const app = express();
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const PORT = process.env.PORT || 8080;
