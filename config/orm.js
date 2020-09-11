8455cc1a2af4cc48c896dad86b4ea32cc83577ba
SS Jones <SSJones.code@gmail.com>
2020-09-10 21:45:42 -0700

    I added the insert and update function definitions to the orm.js file.

--- config/orm.js
+++ config/orm.js
@@ -21,7 +21,46 @@ const orm = {
       cb(result);
     });
   },
-};
 
+
+insertOne: function(table, cols, vals, cb) {
+  var queryString = "INSERT INTO " + table;
+
+  queryString += " (";
+  queryString += cols.toString();
+  queryString += ") ";
+  queryString += "VALUES (";
+  queryString += printQuestionMarks(vals.length);
+  queryString += ") ";
+
+  console.log(queryString);
+
+  connection.query(queryString, vals, function(err, result) {
+    if (err) {
+      throw err;
+    }
+
+    cb(result);
+  });
+},
+// An example of objColVals would be {name: panther, sleepy: true}
+updateOne: function(table, objColVals, condition, cb) {
+  var queryString = "UPDATE " + table;
+
+  queryString += " SET ";
+  queryString += objToSql(objColVals);
+  queryString += " WHERE ";
+  queryString += condition;
+
+  console.log(queryString);
+  connection.query(queryString, function(err, result) {
+    if (err) {
+      throw err;
+    }
+
+    cb(result);
+  });
+},
+};
 //    * Export the ORM object in `module.exports`.
 module.exports = connection;
