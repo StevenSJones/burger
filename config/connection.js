617eaf84d1134d81dfef1bc89e95a2e04b1822ef
SS Jones <SSJones.code@gmail.com>
2020-09-16 17:36:23 -0700

    I added some commenting in the server.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // #### Config Setup
 //import our npm packages
 const mysql = require("mysql");
-//this describes the connection to-be of the sql server(dialing the number to make the connection)
+//this describes the connection to-be of the sql server(dialing the number to make the connection but not pressing green call button)
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
